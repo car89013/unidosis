bd2fb675dda2049dad64cb031142500dd3f4ef89
car89013 <cpachecoj@hotmail.com>
2018-04-24 18:24:00 -0600

    24/04/2018 cpj

--- RestApi/Unidosis.WebApi/Global.asax.cs
+++ RestApi/Unidosis.WebApi/Global.asax.cs
@@ -6,6 +6,7 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Unidosis.WebApi.App_Start;
 
 namespace Unidosis.WebApi
 {
@@ -18,6 +19,7 @@ namespace Unidosis.WebApi
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            GlobalConfiguration.Configuration.MessageHandlers.Add(new CORSHandler());
         }
     }
 }
