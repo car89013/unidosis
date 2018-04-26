9dda3a7f4d929d68312bbf4e6beb8e2abf308f6d
aduhtoy <aduthoy@gmail.com>
2018-04-26 17:59:04 -0600

    ADG: se refactoriza prescripcion por atencionpacientes y se incorpora la información de sala y motivo de ingreso

--- Site/src/app/models/atencion-pacientes.ts
+++ Site/src/app/models/atencion-pacientes.ts
@@ -1,14 +1,8 @@
 import {Pacientes} from './pacientes';
 import {BitacoraPacientes} from './bitacora-pacientes';
 import {Asignacioncamas} from './asignacioncamas';
+import {Ingresos} from './ingresos';
 
 export class AtencionPacientes {
-  id: number;
-  paciente: Pacientes;
-  fk_paciente: string;
-  num_brazalete: string;
-  fecha_ingreso: string;
-  fecha_egreso: string;
-  bitacora: BitacoraPacientes[];
-  asignacion: Asignacioncamas;
+
 }
--- Site/src/app/models/ingresos.ts
+++ Site/src/app/models/ingresos.ts
@@ -1,2 +1,16 @@
+import {Pacientes} from './pacientes';
+import {BitacoraPacientes} from './bitacora-pacientes';
+import {Asignacioncamas} from './asignacioncamas';
+
 export class Ingresos {
+  id: number;
+  paciente: Pacientes;
+  fk_paciente: string;
+  num_brazalete: string;
+  fecha_ingreso: string;
+  fecha_egreso: string;
+  motivoIngreso: string;
+  bitacora: BitacoraPacientes[];
+  asignacion: Asignacioncamas;
+
 }
